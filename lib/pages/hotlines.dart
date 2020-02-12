ae18d6bad70a4e43a52f2ff89ea23beb2bb7379b
Alex Levine <alexrlev@gmail.com>
2020-02-12 14:39:25 -0500

    finalizing UrlLauncher
    It works!

--- lib/pages/hotlines.dart
+++ lib/pages/hotlines.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:heroes_manual/utility/hm_appbar.dart';
 import 'package:heroes_manual/utility/hm_bottom_navbar.dart';
-import 'package:heroes_manual/utility/colors.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Hotlines extends StatelessWidget {
@@ -30,26 +29,22 @@ class Hotlines extends StatelessWidget {
               shrinkWrap: true,
               children: <Widget>[
                 ListTile(
-                  // i have no idea if this works because
-                  // my emulator still isn't running:(
-                  // should work according to stack overflow
-                  // i used my phone number so i could test eventually
                   leading: Icon(Icons.phone),
                   title: new FlatButton(
                       onPressed: () => launch("tel://6789564895"),
-                      child: new Text("Call me")),
+                      child: new Text("This is Alex's phone #")),
                 ),
                 ListTile(
                   leading: Icon(Icons.phone),
                   title: new FlatButton(
                       onPressed: () => launch("tel://6789564895"),
-                      child: new Text("Call me")),
+                      child: new Text("Just didn't want to accidentally call a real hotline")),
                 ),
                 ListTile(
                   leading: Icon(Icons.phone),
                   title: new FlatButton(
                       onPressed: () => launch("tel://6789564895"),
-                      child: new Text("Call me")),
+                      child: new Text("Call Alex")),
                 ),
               ],
             ),
