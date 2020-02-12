c83e46ea55f67c90079a13339d173b55a6b37189
Justin Ziegler <justinziegler345@gmail.com>
2020-02-12 00:15:06 -0500

    fixed the lesson package change

--- lib/pages/main_menu.dart
+++ lib/pages/main_menu.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:heroes_manual/utility/hm_appbar.dart';
 import 'package:heroes_manual/utility/hm_bottom_navbar.dart';
 import 'package:heroes_manual/pages/quiz_start.dart';
-import 'package:heroes_manual/pages/lesson_start.dart';
+import 'package:heroes_manual/pages/lesson/lesson_start.dart';
 import 'package:heroes_manual/pages/perpetrator_present.dart';
 import 'package:heroes_manual/pages/about.dart';
 import 'package:heroes_manual/pages/screening_page.dart';
