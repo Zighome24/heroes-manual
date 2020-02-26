7e467886ba3a0fcfc3cb02a4503133e06335d31b
Paula Ruiz <paularuiz22@gmail.com>
2020-02-26 10:15:51 -0500

    make main menu stateful

--- lib/pages/main_menu.dart
+++ lib/pages/main_menu.dart
@@ -5,16 +5,33 @@ import 'package:heroes_manual/utility/hm_bottom_navbar.dart';
 
 class MainMenu extends StatefulWidget {
   static const String route = '/fancy_main_menu';
-  // TODO: add flutter loading icon for app startup - might throw an error if the waitng is so short - look at LessonStart for example with empty lesson, if empty - show empty container, that's where the load icon would be I think. Once it loads, set the state
+
+  // TODO: add flutter loading icon for app startup - might throw an error if the waiting is so short
+  // look at LessonStart for example with empty lesson, if empty - show empty container, that's where the load icon would be I think. Once it loads, set the state
   // TODO: it will just be loading quiz and lesson names - comes in as a map, just need to get the keys
   // TODO: ignore icons for cards for now
+
+  @override
+  State createState() => _MainMenuState();
+}
+
+class _MainMenuState extends State<MainMenu> {
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HMAppBar(
         showTrailing: true,
       ),
-      // TODO: implement body
+      body: SafeArea(
+        child: ListView(
+          children: <Widget>[
+            Container(
+
+            ),
+          ]
+        ),
+      ),
       bottomNavigationBar: HMBottomNavBar(isHomeSelected: true,),
     );
   }
