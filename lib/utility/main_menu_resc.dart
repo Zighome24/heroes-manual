2b0743ae52d255d6e2adbf8132e1647651fd3453
Paula Ruiz <paularuiz22@gmail.com>
2020-02-27 16:35:27 -0500

    create resource page for routes for main menu routes for categorical horizontal scroll section constructors

---
+++ lib/utility/main_menu_resc.dart
@@ -0,0 +1,5 @@
+import 'package:heroes_manual/pages/hotlines.dart';
+import 'package:heroes_manual/pages/laws.dart';
+import 'package:heroes_manual/pages/organizations.dart';
+
+const resourceRoutes = [Organizations.route, Hotlines.route, Laws.route];
