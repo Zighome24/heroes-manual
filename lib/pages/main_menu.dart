bd361bd938f66c75425f79c9f6a81b0fe794bf2a
Sophia Marcus <smarcus7@gatech.edu>
2020-02-27 18:19:07 -0500

    made about page into an overlay on main_menu; needs exit navigation and formatting

--- lib/pages/main_menu.dart
+++ lib/pages/main_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:heroes_manual/utility/colors.dart';
 import 'package:heroes_manual/utility/hm_appbar.dart';
 import 'package:heroes_manual/utility/hm_bottom_navbar.dart';
 import 'package:heroes_manual/pages/organizations.dart';
@@ -18,7 +19,7 @@ class MainMenu extends StatelessWidget {
       appBar: HMAppBar(
         showTrailing: true,
         showLeading: true,
-        leadingAction: () => Navigator.pushNamed(context, About.route),
+        leadingAction: () => showOverlay(context),
         leadingIcon: Icons.info_outline,
       ),
       body: Center(
@@ -74,6 +75,49 @@ class MainMenu extends StatelessWidget {
         ),
       ),
       bottomNavigationBar: HMBottomNavBar(isHomeSelected: true),
+    );
+  }
+
+  showOverlay(BuildContext context) {
+    return Overlay.of(context).insert(
+        OverlayEntry(
+            builder: (context) => Material(
+                type: MaterialType.transparency,
+                child: Container(
+                    constraints: BoxConstraints.expand(),
+                    color: Colors.grey.withOpacity(0.6),
+                    child: Padding(
+                        padding: EdgeInsets.only(top: 85.0, bottom: 65.0, right: 15.0, left: 15.0),
+                        child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                            child: Padding(
+                                padding: EdgeInsets.all(15.0),
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: <Widget>[
+                                      Flexible(
+                                          flex: 3,
+                                          child: Text('Created by Team Helping Our Heroes, whose members '
+                                              + 'are students of the Georgia Institute of Technology, our '
+                                              + 'project, Heroes\' Manual, aims to serve as a quick, '
+                                              + 'intuitive guide that law enforcement officers and other '
+                                              + 'service providers can use for resources and instruction '
+                                              + 'on handling domestic violence situations.',
+                                              //TODO: change the source of the purple color and style to main
+                                              //  or some standardized/uniform font style for the app
+                                              style: new TextStyle(color: purple.shade500, fontSize: 20))
+                                      ),
+                                    ]
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
     );
   }
 }
