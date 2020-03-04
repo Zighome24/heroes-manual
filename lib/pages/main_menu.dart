97d6ef412a52bc9ab3f69e6a78b7e85138635c69
Paula Ruiz <paularuiz22@gmail.com>
2020-03-04 15:12:24 -0500

    change from ListView builder to manual ListView, load futures for Quizzes

--- lib/pages/main_menu.dart
+++ lib/pages/main_menu.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:heroes_manual/pages/lesson/lesson_start.dart';
+import 'package:heroes_manual/pages/quizzes/quiz_start.dart';
 import 'package:heroes_manual/utility/hm_appbar.dart';
 import 'package:heroes_manual/utility/hm_bottom_navbar.dart';
 import 'package:heroes_manual/data/data.dart';
 import 'package:heroes_manual/widgets/categorical_horizontal_scroller.dart';
+import 'package:heroes_manual/utility/main_menu_resc.dart';
 
 class MainMenu extends StatefulWidget {
   static const String route = '/fancy_main_menu';
@@ -19,11 +22,8 @@ class MainMenu extends StatefulWidget {
 
 class _MainMenuState extends State<MainMenu> {
 // going to set the state after loading is done
-  List<String> _trainings = new List<String>();
-  List<String> categories = <String>['Organizations', 'Laws', 'Trainings',
-    'Quizzes', 'Roles'];
-  List<String> exampleElements = <String>['Element1', 'Element2', 'Element3',
-  'Element4', 'Element5', 'Element6', 'Element7', 'Element8'];
+  List<String> _trainings = [];
+  List<String> _quizzes = [];
 
   @override
   Widget build(BuildContext context) {
@@ -33,29 +33,64 @@ class _MainMenuState extends State<MainMenu> {
       });
     });
 
+    Quiz.loadQuizNames().then((quizNames) {
+      setState(() {
+        _quizzes = quizNames;
+      });
+    });
+
     return Scaffold(
-        appBar: HMAppBar(
-          showTrailing: true,
-        ),
+      appBar: HMAppBar(
+        showTrailing: true,
+      ),
       body: SafeArea(
         // TODO: consider using ListView.separated
           child: ClipRect(
             // TODO: pass arguments into CategoricalHoriz to say which
-              child: ListView.separated(
-                  shrinkWrap: true,
-                  scrollDirection: Axis.vertical,
-                  physics: ScrollPhysics(),
-                  padding: const EdgeInsets.all(8),
-                  itemCount: categories.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return CategoricalHorizontalScroller(categories[index], false,
-                     exampleElements);
-                  },
-                separatorBuilder: (BuildContext context, int index) => Divider(
-                  color: Colors.white,
-                  height: 10,
-                  thickness: 10,
-                ),
+              child: ListView(
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                physics: ScrollPhysics(),
+                padding: const EdgeInsets.all(8),
+                children: <Widget>[
+                  CategoricalHorizontalScroller(
+                    categoryTitle: 'Trainings',
+                    routes: false,
+                    elementTitlesOrRoutes: _trainings,
+                    routeName: LessonStart.route,
+                  ),
+                  Divider(
+                    color: Colors.white,
+                    height: 10,
+                    thickness: 10,
+                  ),
+                  CategoricalHorizontalScroller(
+                    categoryTitle: 'Quizzes',
+                    routes: false,
+                    elementTitlesOrRoutes: _quizzes,
+                    routeName: QuizStart.route,
+                  ),
+                  Divider(
+                    color: Colors.white,
+                    height: 10,
+                    thickness: 10,
+                  ),
+                  CategoricalHorizontalScroller(
+                    categoryTitle: 'Resources',
+                    elementTitlesOrRoutes: resourceRoutes,
+                    routes: true,
+                  ),
+                  Divider(
+                    color: Colors.white,
+                    height: 10,
+                    thickness: 10,
+                  ),
+                  CategoricalHorizontalScroller(
+                    categoryTitle: 'Roles',
+                    elementTitlesOrRoutes: rolesNames,
+                    routes: false,
+                  )
+                ],
               )
           )
       ),
