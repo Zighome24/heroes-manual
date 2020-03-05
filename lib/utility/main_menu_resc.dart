4619439ce053371f2bb1022012f4cc46e8cfc3b4
Paula Ruiz <paularuiz22@gmail.com>
2020-03-05 12:57:55 -0500

    add debugging routes for screening-related pages

--- lib/utility/main_menu_resc.dart
+++ lib/utility/main_menu_resc.dart
@@ -1,5 +1,9 @@
 import 'package:heroes_manual/pages/hotlines.dart';
 import 'package:heroes_manual/pages/laws.dart';
 import 'package:heroes_manual/pages/organizations.dart';
+import 'package:heroes_manual/pages/perpetrator_present.dart';
+import 'package:heroes_manual/pages/screening_page.dart';
 
 const resourceRoutes = [Organizations.route, Hotlines.route, Laws.route];
+// TODO: debug routes for screening - remove for production
+const debugScreenRoutes = [Screening.route, PerpetratorPresent.route];
