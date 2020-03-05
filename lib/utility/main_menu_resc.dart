1cf9a40c784f2efee438573bd149ae2862940db7
Paula Ruiz <paularuiz22@gmail.com>
2020-03-05 12:59:54 -0500

    add debug navigation to about page

--- lib/utility/main_menu_resc.dart
+++ lib/utility/main_menu_resc.dart
@@ -3,7 +3,8 @@ import 'package:heroes_manual/pages/laws.dart';
 import 'package:heroes_manual/pages/organizations.dart';
 import 'package:heroes_manual/pages/perpetrator_present.dart';
 import 'package:heroes_manual/pages/screening_page.dart';
+import 'package:heroes_manual/pages/about.dart';
 
 const resourceRoutes = [Organizations.route, Hotlines.route, Laws.route];
 // TODO: debug routes for screening - remove for production
-const debugScreenRoutes = [Screening.route, PerpetratorPresent.route];
+const debugScreenRoutes = [Screening.route, PerpetratorPresent.route, About.route];
