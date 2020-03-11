889858e80082a9f884ddf4243db6d742ca50cca1
Paula Ruiz <paularuiz22@gmail.com>
2020-03-11 13:16:04 -0400

    better center the hotlines, bring text closer to phone icon

--- lib/pages/hotlines.dart
+++ lib/pages/hotlines.dart
@@ -1,11 +1,23 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:heroes_manual/utility/colors.dart';
 import 'package:heroes_manual/utility/hm_appbar.dart';
 import 'package:heroes_manual/utility/hm_bottom_navbar.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class Hotlines extends StatelessWidget {
   static const String route = '/hotlines';
+  static const List<String> hotlineNumbers = [
+    "tel://6789564895",
+    "tel://6789564895",
+    "tel://6789564895"
+  ];
+  static const List<String> hotlineTitles = [
+    "This is Alex's phone #",
+    "not an actual hotline",
+    "Call Alex"
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -23,52 +35,53 @@ class Hotlines extends StatelessWidget {
             Container(
               padding: const EdgeInsets.fromLTRB(30.0, 30.0, 30.0, 0.0),
               child: Text(
-                'Important hotlines.',
-                style: new TextStyle(
-                    color: Colors.deepPurpleAccent,
-                    fontSize: 20
-                ),
+                'Important Hotlines',
+                style:
+                new TextStyle(color: Colors.deepPurpleAccent, fontSize: 20),
               ),
             ),
-            ListView(
-              padding: const EdgeInsets.fromLTRB(20.0, 10.0, 10.0, 10.0),
-              shrinkWrap: true,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                ListTile(
-                  leading: Icon(Icons.phone),
-                  title: new FlatButton(
-                    onPressed: () => launch("tel://6789564895"),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: new Text(
-                        "This is Alex's phone #",
-                        textAlign: TextAlign.left,
-                      ),
-                    ),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.phone),
-                  title: new FlatButton(
-                    onPressed: () => launch("tel://6789564895"),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: new Text(
-                          "didn't want to accidentally call a hotline"
-                      ),
-                    ),
-                  ),
-                ),
-                ListTile(
-                  leading: Icon(Icons.phone),
-                  title: new FlatButton(
-                    onPressed: () => launch("tel://6789564895"),
-                    child: SizedBox(
-                      width: double.infinity,
-                      child: new Text(
-                          "Call Alex"
-                      ),
-                    ),
+                Align(
+                  alignment: Alignment.center,
+                  child: ListView.separated(
+                    padding: const EdgeInsets.fromLTRB(20.0, 10.0, 10.0, 10.0),
+                    shrinkWrap: true,
+                    itemCount: hotlineNumbers.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.all(10.0),
+                        child: GestureDetector(
+                          onTap: () => launch(hotlineNumbers[index]),
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 50.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Icon(Icons.phone),
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Text(
+                                    hotlineTitles[index],
+                                    textAlign: TextAlign.left,
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                    separatorBuilder: (BuildContext context, int index) =>
+                        Divider(
+                            height: 10,
+                            thickness: 10,
+                            color: background,
+                        ),
                   ),
                 ),
               ],
