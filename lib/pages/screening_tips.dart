f7a8cd7b00091075df35936d501558ff3518f56c
Paula Ruiz <paularuiz22@gmail.com>
2020-03-31 10:50:30 -0400

    add divider between tips, add divider between title and list of tips

--- lib/pages/screening_tips.dart
+++ lib/pages/screening_tips.dart
@@ -6,12 +6,16 @@ import 'package:heroes_manual/utility/colors.dart';
 class ScreeningTips extends StatelessWidget {
   static const String route = '/screening_tips';
   static const List<String> screening_tips = [
-    "This is the first example tip.",
-    "This is the second example tip.",
-    "This is the third example tip.",
-    "This is the fourth example tip.",
-    "This is the fifth example tip.",
-    "This is the sixth example tip.",
+    "This is the first example tip. It's a little longer so we can properly"
+        "test out the length of a tip, how it looks when it is longer, "
+        "and whether the font size, color, and contrast are all appropriate.",
+    "This is the second example tip. Like two peas in a pod.",
+    "This is the third example tip. All good things come in threes.",
+    "This is the fourth example tip. May the fourth be with you.",
+    "This is the fifth example tip. Probably better than Fifth Harmony's "
+        "new song.",
+    "This is the sixth example tip. In the sixth grade, social studies was"
+        " my favorite subject.",
     "This is the seventh example tip.",
     "This is the eighth example tip.",
     "This is the ninth example tip.",
@@ -34,7 +38,7 @@ class ScreeningTips extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Container(
-                padding: const EdgeInsets.all(30.0),
+                padding: const EdgeInsets.all(20.0),
                 alignment: Alignment.center,
                 child: Stack(
                   children: <Widget>[
@@ -49,11 +53,14 @@ class ScreeningTips extends StatelessWidget {
                   ],
                 ),
               ),
-
+              Divider(
+                color: background.shade600,
+                thickness: 3.0,
+              ),
               Expanded(
                 child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
-                  child: ListView.builder(
+                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
+                  child: ListView.separated(
                     shrinkWrap: true,
                     scrollDirection: Axis.vertical,
                     physics: ScrollPhysics(),
@@ -62,13 +69,21 @@ class ScreeningTips extends StatelessWidget {
                       return Container(
                         padding: const EdgeInsets.all(10.0),
                         alignment: Alignment.topLeft,
+                        //decoration: BoxDecoration(
+                        //    color: background.shade600
+                        //),
                         child: Text(
                           screening_tips[index],
                           style:
-                          TextStyle(color: purple.shade500, fontSize: 20),
+                          TextStyle(color: purple.shade600, fontSize: 20),
                         ),
                       );
                     },
+                    separatorBuilder: (BuildContext context, int index)
+                    => Divider(
+                      color: background.shade600,
+                      thickness: 3.0,
+                    ),
                   ),
                 ),
               ),
