089fce981ebc0d8ec33e5304bab82c9986cd39cd
Paula Ruiz <paularuiz22@gmail.com>
2020-04-03 12:20:22 -0400

    change 'Tips to Separate' content to be stored as utility string

--- lib/utility/strings.dart
+++ lib/utility/strings.dart
@@ -1,1 +1,18 @@
 const String safety_plan_info = "A safety plan is a personalized, practical plan that details way to stay safe while in a relationship, planning leave a relationship, or after leaving a relationship.\n\nGive this safety plan to the person in need for them to fill out and keep safe.\n\nDon't directly text or email this to a potential victim to avoid potential HIPAA violations.";
+const List<String> screening_tips = [
+  "This is the first example tip. It's a little longer so we can properly"
+      "test out the length of a tip, how it looks when it is longer, "
+      "and whether the font size, color, and contrast are all appropriate.",
+  "This is the second example tip. Like two peas in a pod.",
+  "This is the third example tip. All good things come in threes.",
+  "This is the fourth example tip. May the fourth be with you.",
+  "This is the fifth example tip. Probably better than Fifth Harmony's "
+      "new song.",
+  "This is the sixth example tip. In the sixth grade, social studies was"
+      " my favorite subject.",
+  "This is the seventh example tip.",
+  "This is the eighth example tip.",
+  "This is the ninth example tip.",
+  "This is the tenth example tip.",
+  "This is the eleventh example tip."
+];
