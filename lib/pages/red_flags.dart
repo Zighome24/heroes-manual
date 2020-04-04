0706c1bcb83babfa0ab7f9fd0b47bd2c27cf250d
Paula Ruiz <paularuiz22@gmail.com>
2020-04-04 15:50:36 -0400

    change display of list elements, add display of source, and hide source when there is none

--- lib/pages/red_flags.dart
+++ lib/pages/red_flags.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:heroes_manual/utility/colors.dart';
 import 'package:heroes_manual/utility/hm_appbar.dart';
 import 'package:heroes_manual/utility/hm_bottom_navbar.dart';
 import 'package:heroes_manual/widgets/simple_vertical_titled_list.dart';
@@ -8,14 +9,15 @@ import 'package:heroes_manual/utility/strings.dart';
 class RedFlags extends StatelessWidget {
   static const String route = '/red_flags';
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: HMAppBar(
         showTrailing: true,
         showLeading: true,
-        //leadingAction: () => showOverlay(context),
-        leadingIcon: Icons.info_outline,
+        trailingAction: () => showOverlay(context),
+        trailingIcon: Icons.info_outline,
       ),
       body: SafeArea(
         child: ClipRect(
@@ -29,5 +31,84 @@ class RedFlags extends StatelessWidget {
       ),
       bottomNavigationBar: HMBottomNavBar(),
     );
+  }
+
+  showOverlay(BuildContext context) {
+    OverlayState overlayState = Overlay.of(context);
+    OverlayEntry overlayEntry;
+    overlayEntry =
+//    return Overlay.of(context).insert(
+    OverlayEntry(
+        builder: (context) => Material(
+            type: MaterialType.transparency,
+            child: Container(
+                constraints: BoxConstraints.expand(),
+                color: Colors.grey.withOpacity(0.6),
+                child: Padding(
+                    padding: EdgeInsets.only(top: 85.0, bottom: 65.0, right: 15.0, left: 15.0),
+                    child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.all(Radius.circular(10.0))),
+                        child: Padding(
+                            padding: EdgeInsets.all(15.0),
+                            child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                    crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  Flexible(
+                                      flex: 1,
+                                      child: Row(
+                                          children: <Widget>[
+                                            IconButton(
+                                              onPressed: () => overlayEntry.remove(),
+                                              icon: Icon(
+                                                Icons.arrow_back_ios,
+                                                color: purple.shade500,
+                                              ),
+                                              iconSize: 28,
+                                            ),
+                                            Spacer()
+                                          ]
+                                      )
+                                  ),
+                                  Expanded(
+                                      flex: 9,
+                                      child: Padding(
+                                        padding: EdgeInsets.only(top: 15.0),
+                                        child: SingleChildScrollView(
+                                          child: Column(
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            children: <Widget>[
+                                              Container(
+                                                  padding: const EdgeInsets.all(30.0),
+                                                  child: Text('Sources',
+                                                      style: TextStyle(
+                                                          color: purple.shade500,
+                                                          fontSize: 20))
+                                              ),
+                                              Container(
+                                                  padding: const EdgeInsets.all(30.0),
+                                                  child: Text(source_red_flags,
+                                                      style: TextStyle(
+                                                          color: purple.shade500,
+                                                          fontSize: 20))
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      )
+                                  )
+                                ]
+                            )
+                        )
+                    )
+                )
+            )
+        )
+    );
+    if(source_red_flags.isNotEmpty) {
+      overlayState.insert(overlayEntry);
+    }
   }
 }
--- lib/widgets/simple_vertical_titled_list.dart
+++ lib/widgets/simple_vertical_titled_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:heroes_manual/utility/hm_appbar.dart';
 import 'package:heroes_manual/utility/hm_bottom_navbar.dart';
@@ -23,36 +24,68 @@ class SimpleVerticalTitledList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(pageTitle,
-                style: new TextStyle(
-                    color: purple.shade500,
-                    fontSize: 20
-                )
-            ),
-            Text(pageSubTitle,
-              style: TextStyle(
-                color: purple.shade600,
-                fontSize: 17,
+        child: ClipRect(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(10.0),
+                alignment: Alignment.center,
+                child: Text(pageTitle,
+                    style: new TextStyle(
+                        color: purple.shade500,
+                        fontSize: 35
+                    )
+                ),
               ),
-            ),
-            ListView.separated(
-              shrinkWrap: true,
-              itemCount: listItems.length,
-              scrollDirection: Axis.vertical,
-              physics: ScrollPhysics(),
-              separatorBuilder: (BuildContext context, int index) => Divider(),
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  leading: Icon(Icons.label),
-                  title: Text(listItems[index]),
-                );
-              },
-            ),
-          ],
+              Container(
+                padding: const EdgeInsets.all(5.0),
+                alignment: Alignment.center,
+                child: Text(pageSubTitle,
+                  style: TextStyle(
+                    color: purple.shade600,
+                    fontSize: 20,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              Divider(
+                color: background.shade600,
+                thickness: 3.0,
+              ),
+
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 0.0),
+                  child: ListView.separated(
+                    shrinkWrap: true,
+                    itemCount: listItems.length,
+                    scrollDirection: Axis.vertical,
+                    physics: ScrollPhysics(),
+                    separatorBuilder: (BuildContext context, int index)
+                    => Divider(
+                      color: background.shade600,
+                      thickness: 2.0,
+                    ),
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        padding: const EdgeInsets.all(10.0),
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          listItems[index],
+                          style: TextStyle(
+                              color: purple.shade500,
+                              fontSize: 18
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
