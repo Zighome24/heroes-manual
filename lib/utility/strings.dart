272853e9f9fd1e0b0b18dd96d644801017cc7bbd
Paula Ruiz <paularuiz22@gmail.com>
2020-04-04 08:49:12 -0400

    add content for red flags from reference card

--- lib/utility/strings.dart
+++ lib/utility/strings.dart
@@ -37,3 +37,46 @@ const List<String> not_separate_screening_tips = ["Be careful not to induce "
 const String source_not_separate_screening_tips = "Bloomberg, M. R. (2004). "
     "Medical Providers’Guide to Managing the Care of Domestic Violence Patients"
     " within a Cultural Context.";
+
+const String red_flags_subtitle = "A list of warning signs to indicate a possible abuser:";
+const List<String> red_flags = ["Jealous",
+"Watching the amount of time you are away",
+"Wanting to keep you away from friends",
+"Asks ""Who are you talking to?"" when you are on the phone",
+"Asks too many questions",
+"Sulks when he doesn't get his way",
+"Seems childish and/or insecure",
+"When something is important to you, he tunes you out",
+"He talks louder until he gets your undivided attention",
+"He physically restrains you until you listen to him",
+"Yells in your ear (sometimes until it is painful)",
+"He follows you around endlessly, repeating things over and over again",
+"Makes you repeat what he said back to him to be sure you were listening",
+"Makes you stop what you are doing to listen to him",
+"He makes all the rules. There is no flexibility",
+"What you think and feel are not important",
+"What he thinks and feels is very important",
+"He says, ""We will get along fine as long as it goes my way.""",
+"He won't admit it when he's wrong",
+"He pesters you about it until you admit it",
+"He tells you what your feelings are and then punishes you with the silent treatment or violence",
+"He acts as if he found a clue and makes a big issue of it",
+"He makes frequent accusations",
+"He acts as if any attention to another human being is flirting",
+"He says things like, ""You belong to me"", ""I got papers on you"", and ""You're my wife.""",
+"He tells you how to dress or whether or not to wear make-up.",
+"He criticizes frequently",
+"He uses what you have told him against you",
+"He doesn't like anybody you like or he criticizes your friends",
+"He is controlling",
+"He is possessive of you",
+"He is bossy",
+"He is jobless",
+"He is a drug/alcohol user and uses that as an excuse for his behavior",
+"He was abusive in a previous relationship",
+"He has to know about your business but is secretive about his own",
+"He smiles too much (sneaky)",
+"He is always argumentative",
+"He has an uncontrollable temper"];
+// TODO: fill in red flags source
+const String source_red_flags = "";
