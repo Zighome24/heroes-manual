2c1978a25f73a272f504992a3f47ed28695c0197
Paula Ruiz <paularuiz22@gmail.com>
2020-04-04 15:56:12 -0400

    remove gender-specific language from the red flags

--- lib/utility/strings.dart
+++ lib/utility/strings.dart
@@ -20,63 +20,63 @@ const List<String> screening_tips = [
 const List<String> not_separate_screening_tips = ["Be careful not to induce "
     "conflict between the victim and the abuser, as this may increase his or "
     "her risk of being harmed.",
-"Tell the abuser that it is a standard procedure for each patient to be "
-    "examined alone.",
-"Have a sign stating ""Patients only beyond this point.""",
-"Screen the patient during a time of privacy following a urine test or when "
-    "he or she is being weighed.",
-"Request that the suspected abuser complete some administrative forms while the"
-    " patient is screened in a separate room.",
-"The practitioner could speak to the suspected abuser concerning a routine "
-    "health issue while another member of staff screens the victim.",
-"Use a key word that will indicate the presence of a potential domestic "
-    "violence situation to a staff member. The victim would then be called out "
-    "of the examination room to discuss the possibility of domestic violence,"
-    " while the practitioner remains with the abuser. The abuser is less likely"
-    " to be suspicious if the practitioner remains with him."];
+  "Tell the abuser that it is a standard procedure for each patient to be "
+      "examined alone.",
+  "Have a sign stating ""Patients only beyond this point.""",
+  "Screen the patient during a time of privacy following a urine test or when "
+      "he or she is being weighed.",
+  "Request that the suspected abuser complete some administrative forms while the"
+      " patient is screened in a separate room.",
+  "The practitioner could speak to the suspected abuser concerning a routine "
+      "health issue while another member of staff screens the victim.",
+  "Use a key word that will indicate the presence of a potential domestic "
+      "violence situation to a staff member. The victim would then be called out "
+      "of the examination room to discuss the possibility of domestic violence,"
+      " while the practitioner remains with the abuser. The abuser is less likely"
+      " to be suspicious if the practitioner remains with him."];
 const String source_not_separate_screening_tips = "Bloomberg, M. R. (2004). "
     "Medical Providers’Guide to Managing the Care of Domestic Violence Patients"
     " within a Cultural Context.";
 
 const String red_flags_subtitle = "A list of warning signs to indicate a possible abuser:";
 const List<String> red_flags = ["Jealous",
-"Watching the amount of time you are away",
-"Wanting to keep you away from friends",
-"Asks ""Who are you talking to?"" when you are on the phone",
-"Asks too many questions",
-"Sulks when he doesn't get his way",
-"Seems childish and/or insecure",
-"When something is important to you, he tunes you out",
-"He talks louder until he gets your undivided attention",
-"He physically restrains you until you listen to him",
-"Yells in your ear (sometimes until it is painful)",
-"He follows you around endlessly, repeating things over and over again",
-"Makes you repeat what he said back to him to be sure you were listening",
-"Makes you stop what you are doing to listen to him",
-"He makes all the rules. There is no flexibility",
-"What you think and feel are not important",
-"What he thinks and feels is very important",
-"He says, ""We will get along fine as long as it goes my way.""",
-"He won't admit it when he's wrong",
-"He pesters you about it until you admit it",
-"He tells you what your feelings are and then punishes you with the silent treatment or violence",
-"He acts as if he found a clue and makes a big issue of it",
-"He makes frequent accusations",
-"He acts as if any attention to another human being is flirting",
-"He says things like, ""You belong to me"", ""I got papers on you"", and ""You're my wife.""",
-"He tells you how to dress or whether or not to wear make-up.",
-"He criticizes frequently",
-"He uses what you have told him against you",
-"He doesn't like anybody you like or he criticizes your friends",
-"He is controlling",
-"He is possessive of you",
-"He is bossy",
-"He is jobless",
-"He is a drug/alcohol user and uses that as an excuse for his behavior",
-"He was abusive in a previous relationship",
-"He has to know about your business but is secretive about his own",
-"He smiles too much (sneaky)",
-"He is always argumentative",
-"He has an uncontrollable temper"];
+  "Watching the amount of time you are away",
+  "Wanting to keep you away from friends",
+  "Asks ""Who are you talking to?"" when you are on the phone",
+  "Asks too many questions",
+  "Sulks when they don't get their way",
+  "Seems childish and/or insecure",
+  "When something is important to you, they tune you out",
+  "Talks louder until they get your undivided attention",
+  "Physically restrains you until you listen to them",
+  "Yells in your ear (sometimes until it is painful)",
+  "Follows you around endlessly, repeating things over and over again",
+  "Makes you repeat what he/she said back to them to be sure you were listening",
+  "Makes you stop what you are doing to listen to them",
+  "Makes all the rules. There is no flexibility",
+  "What you think and feel are not important",
+  "What they think and feel is very important",
+  "Says, ""We will get along fine as long as it goes my way.""",
+  "Won't admit it when they're wrong",
+  "Pesters you about it until you admit it",
+  "Tells you what your feelings are and then punishes you with the silent treatment or violence",
+  "Acts as if they found a clue and makes a big issue of it",
+  "Makes frequent accusations",
+  "Acts as if any attention to another human being is flirting",
+  "Says things like, ""You belong to me"", ""I got papers on you"", and ""You're my wife/husband.""",
+  "Tells you how to dress or whether or not to wear make-up.",
+  "Criticizes frequently",
+  "Uses what you have told him against you",
+  "Doesn't like anybody you like or he criticizes your friends",
+  "Is controlling",
+  "Is possessive of you",
+  "Is bossy",
+  "Is jobless",
+  "Is a drug/alcohol user and uses that as an excuse for their behavior",
+  "Was abusive in a previous relationship",`
+  "Has to know about your business but is secretive about his own",
+  "Smiles too much (sneaky)",
+  "Is always argumentative",
+  "Has an uncontrollable temper"];
 // TODO: fill in red flags source
 const String source_red_flags = "";
