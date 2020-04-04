4402827d6ae4b83355e4d929b2fc55efa3a95288
Paula Ruiz <paularuiz22@gmail.com>
2020-04-04 08:48:42 -0400

    further develop scaffold

--- lib/pages/red_flags.dart
+++ lib/pages/red_flags.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:heroes_manual/utility/hm_appbar.dart';
+import 'package:heroes_manual/utility/hm_bottom_navbar.dart';
 import 'package:heroes_manual/widgets/simple_vertical_titled_list.dart';
+import 'package:heroes_manual/utility/strings.dart';
 
 class RedFlags extends StatelessWidget {
+  static const String route = '/red_flags';
 
   @override
   Widget build(BuildContext context) {
@@ -11,14 +14,20 @@ class RedFlags extends StatelessWidget {
       appBar: HMAppBar(
         showTrailing: true,
         showLeading: true,
-        leadingAction: () => showOverlay(context),
+        //leadingAction: () => showOverlay(context),
         leadingIcon: Icons.info_outline,
       ),
       body: SafeArea(
         child: ClipRect(
-          child: SimpleVerticalTitledList("Red Flags", ),
+          child: SimpleVerticalTitledList(
+            pageTitle: "Red Flags",
+            pageSubTitle: red_flags_subtitle,
+            source: source_red_flags,
+            listItems: red_flags
+          ),
         ),
-      )
+      ),
+      bottomNavigationBar: HMBottomNavBar(),
     );
   }
 }
