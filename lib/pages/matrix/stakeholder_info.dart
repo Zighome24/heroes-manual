9cef4942325f66380279353a3c9fcf8a0bdaaaac
Justin Ziegler <justinziegler345@gmail.com>
2020-04-07 17:52:02 -0400

    Stakeholder Info page completed, draft #1

--- lib/pages/matrix/stakeholder_info.dart
+++ lib/pages/matrix/stakeholder_info.dart
@@ -33,19 +33,20 @@ class StakeholderInfoPageState extends State<StakeholderInfoPage> {
           maxHeight: tabBarHeight
       ),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          IconButton(
+          Expanded(
+            child: FlatButton(
               onPressed: currentState == SIPTabBarState.ATTITUDES
-                ? () { debugPrint("Attitude already choosen"); }
-                : () { setState(() { currentState = SIPTabBarState.ATTITUDES; });},
-              icon: Icon(
+                  ? () { debugPrint("Attitude already choosen"); }
+                  : () { setState(() { currentState = SIPTabBarState.ATTITUDES; });},
+              child: Icon(
                 currentState == SIPTabBarState.ATTITUDES ? MdiIcons.face
                     : MdiIcons.faceOutline,
                 size: tabBarHeight / 2,
                 color: Colors.white,
               )
+            )
           ),
           Container(
             color: Colors.white,
@@ -55,22 +56,22 @@ class StakeholderInfoPageState extends State<StakeholderInfoPage> {
               maxHeight: tabBarHeight,
             ),
           ),
-          IconButton(
+          Expanded(
+            child: FlatButton(
               onPressed: currentState == SIPTabBarState.ACTIONS
-                ? () { debugPrint("Action already choosen"); }
-                : () { setState(() { currentState = SIPTabBarState.ACTIONS; });},
-              icon: Icon(
+                  ? () { debugPrint("Action already choosen"); }
+                  : () { setState(() { currentState = SIPTabBarState.ACTIONS; });},
+              child: Icon(
                 currentState == SIPTabBarState.ACTIONS ? MdiIcons.accountTieVoice
                     : MdiIcons.accountTieVoiceOutline,
                 size: tabBarHeight / 2,
                 color: Colors.white,
               )
-          ),
+            )
+          )
         ],
       ),
     );
-
-    debugPrint(currentState.toString());
 
     return Scaffold(
       appBar: HMAppBar(
@@ -87,13 +88,57 @@ class StakeholderInfoPageState extends State<StakeholderInfoPage> {
             child: tabBar,
           ),
           Expanded(
-            child: currentState == SIPTabBarState.ATTITUDES ?
-            /*ListView.builder(
-              itemBuilder: null,
-            ) : ListView.builder(
-              itemBuilder: null
-            )*/
-            Container() : Container()
+            child: ListView.builder(
+              itemCount: _stakeholder.attitudes.length + 1,
+              shrinkWrap: false,
+              itemBuilder: (BuildContext context, int index) {
+                return index == 0 ? Padding(
+                  padding: EdgeInsets.only(left: 20, top: 20),
+                  child: Text(
+                    currentState == SIPTabBarState.ATTITUDES ? "Attitudes, Perspective, Challenges, Issues, Barriers, Obstacles, and Views" : "Actions",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                        color: Colors.black
+                    ),
+                  ),
+                ) : Padding(
+                  padding: EdgeInsets.all(10),
+                  child: currentState == SIPTabBarState.ATTITUDES ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        _stakeholder.attitudes[index-1].facet,
+                        style: TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                          color: Colors.black
+                        )
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 5, left: 10),
+                        child: Text(
+                          _stakeholder.attitudes[index-1].info,
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.black
+                          )
+                        )
+                      )
+                    ]
+                  ) : Padding(
+                    padding: EdgeInsets.only(left: 10, top: 10),
+                    child: Text(
+                      _stakeholder.actions[index-1],
+                      style: TextStyle(
+                        fontSize: 18
+                      ),
+                    )
+                  ),
+                );
+              },
+            )
           )
         ]
       ),
