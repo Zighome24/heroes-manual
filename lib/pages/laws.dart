cee938570e211e5674749677c757071cf27e856d
Sophia Marcus <smarcus7@gatech.edu>
2020-04-17 22:06:59 -0400

    Laws UI changes

--- lib/pages/laws.dart
+++ lib/pages/laws.dart
@@ -16,15 +16,36 @@ class Laws extends StatelessWidget {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+//          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-                'Will be a list of laws',
-                style: new TextStyle(
-                    color: purple.shade500,
-                    fontSize: 20
-                )
+            Padding(
+              padding: EdgeInsets.all(15),
+              child: IntrinsicHeight(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [purple.shade600, Colors.deepPurple[400]],
+                            begin: Alignment.bottomRight,
+                            end: Alignment.topLeft
+                        ),
+                      ),
+                      child: Text(
+                        'Important Laws',
+                        style: new TextStyle(
+                            color: Colors.white,
+                            fontSize: 30
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
             ListView(
               shrinkWrap: true,
