eba51bb342f82670ceb9d447e81e9b4a7e602c5d
spadi29 <42748341+spadi29@users.noreply.github.com>
2020-04-17 23:23:08 -0400

    fixed organizations UI

--- lib/pages/organizations.dart
+++ lib/pages/organizations.dart
@@ -15,6 +15,7 @@ class Organizations extends StatelessWidget {
     return Scaffold(
       appBar: HMAppBar(
         showLeading: true,
+        showTrailing: true,
         leadingAction: () => Navigator.pop(context),
       ),
       body: Center(
@@ -23,27 +24,50 @@ class Organizations extends StatelessWidget {
             mainAxisSize: MainAxisSize.min,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(20.0),
-                alignment: Alignment.center,
-                child: Stack(
-                  children: <Widget>[
-                    // may need to switch order of these children as render
-                    // order of stacks affects click-ability
-                    Center(
-                      child: Text(
-                        'Important Organizations',
-                        style: TextStyle(color: purple.shade500, fontSize: 30),
-                        textAlign: TextAlign.center,
-                      ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  decoration: new BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [purple.shade600, Colors.deepPurple[400]],
+                        begin: Alignment.bottomRight,
+                        end: Alignment.topLeft
                     ),
-                  ],
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text(
+                      'Tips to Separate Victim and Perpetrator',
+                      style: new TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
                 ),
               ),
-              Divider(
-                color: background.shade600,
-                thickness: 3.0,
-              ),
+//              Container(
+//                padding: const EdgeInsets.all(20.0),
+//                alignment: Alignment.center,
+//                child: Stack(
+//                  children: <Widget>[
+//                    // may need to switch order of these children as render
+//                    // order of stacks affects click-ability
+//                    Center(
+//                      child: Text(
+//                        'Tips to Separate Victim and Perpetrator',
+//                        style: TextStyle(color: purple.shade500, fontSize: 30),
+//                        textAlign: TextAlign.center,
+//                      ),
+//                    ),
+//                  ],
+//                ),
+//              ),
+//              Divider(
+//                color: background.shade600,
+//                thickness: 3.0,
+//              ),
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(15.0, 0.0, 15.0, 0.0),
@@ -63,6 +87,7 @@ class Organizations extends StatelessWidget {
                           organizationTitles[index],
                           style:
                           TextStyle(color: purple.shade600, fontSize: 20),
+                          textAlign: TextAlign.center,
                         ),
                       );
                     },
