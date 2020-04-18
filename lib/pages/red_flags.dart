b3ee44761bb25850450be55f0f15c4c1da459765
Sophia Marcus <smarcus7@gatech.edu>
2020-04-17 22:24:47 -0400

    Simple_Vertical_Titled_List and Red Flags UI changes

--- lib/pages/red_flags.dart
+++ lib/pages/red_flags.dart
@@ -21,12 +21,15 @@ class RedFlags extends StatelessWidget {
       ),
       body: SafeArea(
         child: ClipRect(
-          child: SimpleVerticalTitledList(
-            pageTitle: "Red Flags",
-            pageSubTitle: red_flags_subtitle,
-            source: source_red_flags,
-            listItems: red_flags
-          ),
+          child: Container(
+
+            child: SimpleVerticalTitledList(
+                pageTitle: "Red Flags",
+                pageSubTitle: red_flags_subtitle,
+                source: source_red_flags,
+                listItems: red_flags
+            ),
+          )
         ),
       ),
       bottomNavigationBar: HMBottomNavBar(),
